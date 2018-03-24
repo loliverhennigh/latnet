f9d38a5bfd0dd2a03f2bccbdabdd186b66fae128
loliverhennigh <loliverhennigh101@gmail.com>
2018-03-24 16:31:07 -0400

    text histogram

---
+++ latnet/utils/text_histogram.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+def vector_to_text_hist(vector, bins=10):
+  hist_vector, _ = np.histogram(vector, bins=bins, density=True)
+  hist_vector = 100*hist_vector/np.sum(hist_vector)
+  string = ''
+  for i in xrange(bins):
+    string += str(int(hist_vector[i])).zfill(2) + ', '
+  return string
+  
+
+
